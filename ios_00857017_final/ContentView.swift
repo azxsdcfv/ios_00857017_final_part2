f08a57f7e50857f8a448b8c366286496703cfd2c
9joanna <jcohaonuna@gmail.com>
2021-12-25 05:00:32 +0800

    Initial Commit

---
+++ ios_00857017_final/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios_00857017_final
+//
+//  Created by User14 on 2021/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios_00857017_final/ios_00857017_finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_00857017_finalApp.swift
+//  ios_00857017_final
+//
+//  Created by User14 on 2021/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ios_00857017_finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
